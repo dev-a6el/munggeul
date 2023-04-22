ffccdc3c5e7dbdf20e741f14f54ab90c46787a2c
AbelRyu <a6elryu@gmail.com>
2023-04-22 21:47:42 +0900

    onboard

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p> ㅁ 도메인팔지말지 고민중 </p>
+        <p> ㅁ next.js로 할지 리액트에 노드익스프레스로만 할지 고민중 </p>
         <a
           className="App-link"
           href="https://reactjs.org"
